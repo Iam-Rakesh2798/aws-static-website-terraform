86ee350209f62444018898c01807ac92112abaf2
Iam-Rakesh2798 <raki2798@gmail.com>
2026-06-09 17:49:29 +0530

    infra: add core terraform files and gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "website" {
+  bucket = "rakesh-cloud-2798-demo-2026"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
